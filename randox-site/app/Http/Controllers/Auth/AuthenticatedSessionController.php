38130c4e1bb9173dc1e32885d39d9adcd2b535ff
subskill-quentin <91881904+subskill-quentin@users.noreply.github.com>
2023-06-04 22:50:59 +0200

    git stash

--- randox-site/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ randox-site/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -9,6 +9,8 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
 
 class AuthenticatedSessionController extends Controller
 {
@@ -27,10 +29,23 @@ class AuthenticatedSessionController extends Controller
     {
         $request->authenticate();
 
+        // Récupérez l'utilisateur connecté
+        $user = $request->user();
+
+        // Générez le token JWT
+        $token = JWTAuth::fromUser($user);
+
+        var_dump($token);
+        die();
+        // Stockez le token JWT dans une session, un cookie ou tout autre mécanisme de votre choix
+        // Par exemple, pour stocker le token JWT dans une session Laravel :
+        session(['jwt_token' => $token]);
+
         $request->session()->regenerate();
 
         return redirect()->intended(RouteServiceProvider::HOME);
     }
+
 
     /**
      * Destroy an authenticated session.
